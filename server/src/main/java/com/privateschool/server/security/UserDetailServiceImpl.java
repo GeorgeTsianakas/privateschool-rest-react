f5efa98e01590be8aecdb42b76c821c82d15ec32
George <gtsianakas94@gmail.com>
2020-07-26 19:04:59 +0300

    Spring Security Commit
    Add Spring Security

--- server/src/main/java/com/privateschool/server/security/UserDetailServiceImpl.java
+++ server/src/main/java/com/privateschool/server/security/UserDetailServiceImpl.java
@@ -1,16 +1,34 @@
 package com.privateschool.server.security;
 
+import com.privateschool.server.model.User;
+import com.privateschool.server.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private IUserRepository userRepository;
+
+    //Authorization request will be handled by filters then current user will be checked by here before rest controller.
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        //Granted roles.
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(), grantedAuthorities);
     }
 
 }
